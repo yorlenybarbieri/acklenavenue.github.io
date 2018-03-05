461263af668708798aed3c2ebe3a2b51f00e8bf3
Dennis Molina <dennismolina.17@gmail.com>
2018-03-05 17:30:38 -0600

    Highlight category when selected

--- js/team.js
+++ js/team.js
@@ -6,10 +6,30 @@ var element = document.querySelector('.my-shuffle-container');
 var sizer = element.querySelector('.member-card');
 
 var shuffleInstance = new Shuffle(element, {
-  itemSelector: '.picture-item', sizer: sizer
+  itemSelector: '.picture-item', 
+  sizer: sizer,
+  isCentered: true
 });
 
 
 function filterTeam(tag) {
-  shuffleInstance.filter(tag);
+    setActiveCategorie(tag)
+    shuffleInstance.filter(tag);
+}
+
+function setActiveCategorie(tag) {
+  // loop through all items and remove active class
+  var items = document.getElementsByClassName('btn btn--ghost');
+  console.log("items: ", items.length)
+  for(var i=0; i < items.length; i++) {
+    items[i].setAttribute('class', 'btn btn--ghost');
+  }
+
+  // set the selected tag's item to active
+  var formatedTag = tag.split(" ").join("-").toLowerCase()
+  var item = document.getElementById(formatedTag + '-item');
+  console.log("Item: ", item)
+  if(item) {
+    item.setAttribute('class', 'btn btn--ghost btn--ghost-active');
+  }
 }
