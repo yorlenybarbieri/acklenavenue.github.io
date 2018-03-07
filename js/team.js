921a1612d73492a19fe9dbc4c3bf39385c826e3a
Dennis Molina <dennismolina.17@gmail.com>
2018-03-07 10:38:27 -0600

    Change title when selecting a category

--- js/team.js
+++ js/team.js
@@ -16,13 +16,14 @@ var shuffleInstance = new Shuffle(element, {
 
 function filterTeam(tag) {
     setActiveCategorie(tag)
+    setCategoryTitle(tag)
     shuffleInstance.filter(tag);
 }
 
 function setActiveCategorie(tag) {
   // loop through all items and remove active class
   var items = document.getElementsByClassName('btn btn--ghost');
-  console.log("items: ", items.length)
+
   for(var i=0; i < items.length; i++) {
     items[i].setAttribute('class', 'btn btn--ghost');
   }
@@ -30,8 +31,14 @@ function setActiveCategorie(tag) {
   // set the selected tag's item to active
   var formatedTag = tag.split(" ").join("-").toLowerCase()
   var item = document.getElementById(formatedTag + '-item');
-  console.log("Item: ", item)
+
   if(item) {
     item.setAttribute('class', 'btn btn--ghost btn--ghost-active');
   }
+}
+
+function setCategoryTitle(tag){
+
+    $(".teamlist__title").text(tag);
+
 }
